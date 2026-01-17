be7863ea6f81a8f1026b080e1f87ab9fc24de788
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-01-17 20:07:43 -0300

    Adding packages, registering with the DI container, and route adjustments

--- DataStream.API/Program.cs
+++ DataStream.API/Program.cs
@@ -1,12 +1,25 @@
-using DataStream.API.Database;
+using DataStream.API.Database.Dapper;
+using DataStream.API.Database.Dapper.Interfaces;
+using DataStream.API.Database.EF;
+using DataStream.API.Infra.Settings;
+using DataStream.API.Repositories.Dapper;
+using DataStream.API.Repositories.Interfaces;
+using DataStream.API.Services;
+using DataStream.API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<ConnectionStringSetting>(
+    builder.Configuration.GetSection("ConnectionStrings"));
+
 builder.Services.AddDbContext<DataStreamDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSingleton<IDbConnectionFactory, SqlConnectionFactory>();
 
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IClientRepository, DClientRepository>();
 
 builder.Services.AddControllers();
 
