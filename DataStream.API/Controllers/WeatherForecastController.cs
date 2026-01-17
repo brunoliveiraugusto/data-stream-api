fe8e0beada7a099f5a0914ec817d64049e1a250b
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-01-17 13:08:15 -0300

    initial commit

---
+++ DataStream.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataStream.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
