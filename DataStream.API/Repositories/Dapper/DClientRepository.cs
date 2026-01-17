520e8dc3df2f816c5cd08fe8da432c6cd7064ac6
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-01-17 20:06:30 -0300

    repository for client using Dapper.

---
+++ DataStream.API/Repositories/Dapper/DClientRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using DataStream.API.Database.Dapper.Interfaces;
+using DataStream.API.Models;
+using DataStream.API.Repositories.Interfaces;
+
+namespace DataStream.API.Repositories.Dapper
+{
+    public class DClientRepository(IDbConnectionFactory connectionFactory) : IClientRepository
+    {
+        private readonly IDbConnectionFactory _connectionFactory = connectionFactory;
+
+        public async IAsyncEnumerable<Client> GetStreamingClientsAsync()
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+
+            const string sql = @"
+                SELECT
+                    ClientId,
+                    FullName,
+                    Email,
+                    CreatedAt
+                FROM Clients;
+            ";
+
+            var clients = connection.Query<Client>(
+                sql: sql,
+                buffered: false
+            );
+
+            foreach (var client in clients)
+                yield return client;
+        }
+
+        public async Task<IEnumerable<Client>> GetClients()
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+
+            const string sql = @"
+                SELECT
+                    ClientId,
+                    FullName,
+                    Email,
+                    CreatedAt
+                FROM Clients;
+            ";
+
+            return await connection.QueryAsync<Client>(sql);
+        }
+    }
+}
