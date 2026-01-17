e5edd29538fb93d74f3f67958ec151d6205db9ca
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-01-17 20:02:49 -0300

    factory for building database connection instances

---
+++ DataStream.API/Database/Dapper/Interfaces/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace DataStream.API.Database.Dapper.Interfaces
+{
+    public interface IDbConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+    }
+}
---
+++ DataStream.API/Database/Dapper/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using DataStream.API.Database.Dapper.Interfaces;
+using DataStream.API.Infra.Settings;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace DataStream.API.Database.Dapper
+{
+    public class SqlConnectionFactory(IOptions<ConnectionStringSetting> opt) : IDbConnectionFactory
+    {
+        private readonly string _connectionString = opt.Value.DefaultConnection;
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
