50b4637cb550135ac6893b1a1dc9fef1b1b8ef45
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-01-17 21:09:06 -0300

    Added API for data consumption

---
+++ Consumer.API/Controllers/ClientController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consumer.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+    }
+}
